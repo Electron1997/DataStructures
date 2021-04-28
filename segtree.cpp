3a01557620f3b6b3f946379735bd27907c43a8c9
Electron1997 <63196848+Electron1997@users.noreply.github.com>
2021-04-28 21:14:20 +0200

    Improved segtree

--- segtree.cpp
+++ segtree.cpp
@@ -26,12 +26,12 @@ auto duration = chrono::duration_cast<chrono::microseconds>(stop - start);
 cout << duration.count() << endl;
 */
 
-// N : number of nodes in the segtree (2^20 = 1048576)
-template<class type = ll, int N = 1048576>
+// N : maximum number of elements in the segtree
+template<class type = ll, int N = 100005>
 struct segtree{
 
 	// E: neutral element (set by build_tree())
-	type E, tree[N];
+	type E, tree[N << 1];
 
 	// Associative operation
 	type op(const type& a, const type& b){
@@ -39,42 +39,37 @@ struct segtree{
 	}
 
 	// Time: O(N)
-	void build_tree(type a[], int n, type e, bool pow2 = false){
+	void build_tree(type a[], int n, type e){
 		E = e;
-		int M = N / 2;
 		for(int i = 0; i < n; ++i){
-			tree[M + i] = a[i];
+			tree[N + i] = a[i];
 		}
-		for(int i = M + n; i < N; ++i){
+		for(int i = N + n; i < N << 1; ++i){
 			tree[i] = E;
 		}
-		for(int i = M - 1; i > 0; --i){
-			tree[i] = op(tree[2 * i], tree[2 * i + 1]);
+		for(int i = N - 1; i > 0; --i){
+			tree[i] = op(tree[i << 1], tree[i << 1 | 1]);
 		}
 	}
 
 	// Gets value for range [l, r[ (Time: O(log(N)))
-	type range(int l, int r, int L = 0, int R = N / 2, int i = 1){
-		if(r <= L || l >= R){
-			return E;
-		}
-		if(l <= L && r >= R){
-			return tree[i];
-		}
-		int M = (L + R) / 2;
+	type range(int l, int r){
 		type val = E;
-		val = op(val, range(l, min(M, r), L, M, i * 2));
-		val = op(val, range(max(M, l), r, M, R, i * 2 + 1));
+		for(l += N, r += N; l < r; l >>= 1, r >>= 1){
+			if(l & 1){
+				val = op(val, tree[l++]);
+			}
+			if(r & 1){
+				val = op(val, tree[--r]);
+			}
+		}
 		return val;
 	}
 
 	// Sets i-th element to v (Time: O(log(N)))
 	void update(int i, type v){
-		i += N / 2;
-		tree[i] = v;
-		while(i >= 2){
-			i /= 2;
-			tree[i] = op(tree[i * 2], tree[i * 2 + 1]);
+		for(tree[i += N] = v; i > 0; i >>= 1){
+			tree[i >> 1] = op(tree[i], tree[i ^ 1]);
 		}
 	}
 };
