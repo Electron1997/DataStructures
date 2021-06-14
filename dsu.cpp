5d16fcdf4b64bb35cd423d6a9bf20ef203026ac9
Electron1997 <63196848+Electron1997@users.noreply.github.com>
2021-06-14 02:54:03 +0200

    Made struct

--- dsu.cpp
+++ dsu.cpp
@@ -26,51 +26,75 @@ auto duration = chrono::duration_cast<chrono::microseconds>(stop - start);
 cout << duration.count() << endl;
 */
 
-// Maximum number of elements in the DSU
-const int N = 100000;
+template<int N = 100005>
+struct dsu {
 
-int par[N], h[N];
+	int par[N], h[N];
 
-// Time: O(n)
-void init(int n = N){
-    loop(i, n){
-        par[i] = i;
-        h[i] = 1;
-    }
-}
+	dsu(int n = N){
+		for(int i = 0; i < n; ++i){
+			par[i] = i;
+			h[i] = 1;
+		}
+	}
 
-// Time: ~O(1)
-int find(int i){
-    if(par[i] != i){
-        par[i] = find(par[i]);
-    }
-    return par[i];
-}
+	// Time: ~O(1)
+	int find(int i){
+	    if(par[i] != i){
+	        par[i] = find(par[i]);
+	    }
+	    return par[i];
+	}
 
-// Time: ~O(1)
-void join(int i, int j){
-    int s = find(i);
-    int t = find(j);
-    if(s != t){
-        if(h[s] > h[t]){
-            swap(s, t);
-        }
-        par[s] = t;
-        if(h[s] == h[t]){
-            ++h[t];
-        }
-    }
-}
+	// Time: ~O(1)
+	void join(int i, int j){
+	    int s = find(i);
+	    int t = find(j);
+	    if(s != t){
+	        if(h[s] > h[t]){
+	            swap(s, t);
+	        }
+	        par[s] = t;
+	        if(h[s] == h[t]){
+	            ++h[t];
+	        }
+	    }
+	}
+
+	// Time: O(n)
+	void reset(int n = N){
+	    loop(i, n){
+	        par[i] = i;
+	        h[i] = 1;
+	    }
+	}
+
+};
 
 int main(){
 	/*	// IO
 	ios_base::sync_with_stdio(false);	// unsync C- and C++-streams (stdio, iostream)
 	cin.tie(NULL);	// untie cin from cout (no automatic flush before read)
 	*/
-    int T;
-    cin >> T;
-    loop(t, T){
-        // Solve test case here
+    int n, m, id = 1;
+    cin >> n >> m;
+    dsu<200005> *uf = new dsu<200005>();
+    unordered_map<int, int> mp;
+    int sol = n;
+    loop(i, m){
+    	int u, v;
+    	cin >> u >> v;
+    	if(!mp[u]){
+    		mp[u] = id++;
+    	}
+    	if(!mp[v]){
+    		mp[v] = id++;
+    	}
+    	if(uf->find(mp[u]) != uf->find(mp[v])){
+    		--sol;
+    	}
+    	uf->join(mp[u], mp[v]);
     }
+    cout << sol << endl;
     return 0;
 }
