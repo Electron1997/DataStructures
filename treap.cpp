6909a0d1a4ff8d211487b4c454b45d55f2506038
Mirco Paul <63196848+Electron1997@users.noreply.github.com>
2022-11-16 19:03:36 +0100

    Implemented get, rank, split and join
    Probably contains bugs

--- treap.cpp
+++ treap.cpp
@@ -13,93 +13,214 @@ using namespace std;
 typedef long long ll;
 typedef unsigned long long ull;
 
-/*  // RANDOM NUMBER GENERATOR
-// rng() generates u.a.r. from [0, 2^32 - 1]
-mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
-*/
+// Implements multiset semantics
+namespace treap{    // prevent conflicts with std
 
-// Implements set semantics
-template<class type = int>
-struct treap{
+    // RANDOM NUMBER GENERATOR
+    // rng() generates u.a.r. from [0, 2^32 - 1]
+    mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 
+    template<typename T = int>
     struct node{
-        type data;
-        int priority;
+
+        T data;
+        unsigned prio, size, count;
         node *left, *right;
 
-        node(type val, int prio) : data(val), priority(prio), left(NULL), right(NULL) {}
+        node(T value, unsigned priority) : data(value), prio(priority), size(1), count(1), left(NULL), right(NULL){}
+
     };
 
-    mt19937 rng;
-
-    node *root;
-
-    treap() : rng(mt19937(chrono::steady_clock::now().time_since_epoch().count())), root(NULL) {}
-
-    inline node *rotate_left(node *pivot){
-        node *new_root = pivot->right;
-        pivot->right = new_root->left;
-        new_root->left = pivot;
-        return new_root;
-    }
-
-    inline node *rotate_right(node *pivot){
-        node *new_root = pivot->left;
-        pivot->left = new_root->right;
-        new_root->right = pivot;
-        return new_root;
-    }
-
-    node *insert(type val, node *root){
+    template<typename T = int>
+    inline unsigned size(node<T> *root){
         if(!root){
-            return new node(val, rng());
+            return 0;
         }
-        if(val < root->data){
-            root->left = insert(val, root->left);
-            if(root->priority < root->left->priority){
-                return rotate_right(root);
+        return root->size;
+    }
+
+    // Update all variables that depend on children or count (is called whenever children or count could have changed)
+    template<typename T = int>
+    inline void update(node<T> *root){
+        if(root){
+            root->size = size(root->left) + root->count + size(root->right);
+        }
+    }
+
+    template<typename T = int>
+    inline node<T> *rotate_left(node<T> *root){
+        node<T> *new_root = root->right;
+        root->right = new_root->left;
+        new_root->left = root;
+        return new_root;
+    }
+
+    template<typename T = int>
+    inline node<T> *rotate_right(node<T> *root){
+        node<T> *new_root = root->left;
+        root->left = new_root->right;
+        new_root->right = root;
+        return new_root;
+    }
+
+    template<typename T = int>
+    node<T> *insert(T value, unsigned priority, node<T> *root){
+        if(!root){
+            return new node<T>(value, priority);
+        }
+        if(value == root->data){
+            ++root->count;
+        }else if(value < root->data){
+            root->left = insert(value, priority, root->left);
+            if(root->left->prio > root->prio){
+                root = rotate_right(root);
+                update(root->right);
             }
-        }else if(val > root->data){
-            root->right = insert(val, root->right);
-            if(root->priority < root->right->priority){
-                return rotate_left(root);
+        }else{
+            root->right = insert(value, priority, root->right);
+            if(root->right->prio > root->prio){
+                root = rotate_left(root);
+                update(root->left);
+            }
+        }
+        update(root);
+        return root;
+    }
+
+    template<typename T = int>
+    inline node<T> *insert(T value, node<T> *root){
+        unsigned priority = rng();
+        if(priority) --priority;    // reserve UINT_MAX for special operations
+        return insert(value, priority, root);
+    }
+
+    template<typename T = int>
+    node<T> *find(T value, node<T> *root){
+        while(root && root->data != value){
+            if(value < root->data){
+                root = root->left;
+            }else{
+                root = root->right;
             }
         }
         return root;
     }
 
-    inline void insert(type val){
-        root = insert(val, root);
+    template<typename T = int>
+    node<T> *get(unsigned index, node<T> *root){
+        if(!root){
+            return NULL;
+        }
+        unsigned left_size = size(root->left);
+        if(left_size == index){
+            return root;
+        }
+        if(left_size > index){
+            return get(index, root->left);
+        }
+        return get(index - left_size - 1, root->right);
     }
 
-    inline void remove(type val){}
+    template<typename T = int>
+    int rank(T value, node<T> *root){
+        if(!root){
+            return -1;
+        }
+        if(value < root->data){
+            return rank(value, root->left);
+        }
+        if(value > root->data){
+            return size(root->left) + root->count + rank(value, root->right);
+        }
+        return size(root->left);
+    }
 
-    inline node *find(type val, node *root){
-        while(root){
-            if(val < root->data){
-                root = root->left;
-            }else if(val > root->data){
-                root = root->right;
+    template<typename T = int>
+    node<T> *remove(T value, node<T> *root){
+        if(!root){
+            return NULL;
+        }
+        if(value < root->data){
+            root->left = remove(value, root->left);
+        }else if(value > root->data){
+            root->right = remove(value, root->right);
+        }else{
+            if(root->count > 1){
+                --root->count;
             }else{
-                return root;
+                // Ugly part
             }
         }
-        return NULL;
+        // TO DO: when do we call update()?
     }
 
-    inline void join(treap<type> other){}
+    template<typename T = int>
+    std::pair<node<T>*, node<T>*> split(T value, node<T> *root);
 
-    inline void split(type val){}
-
-    void print_data(node *root){
-        if(!root){
-            return;
+    template<typename T = int>
+    std::pair<node<T>*, node<T>*> split(unsigned index, node<T> *root){
+        unsigned left_size = size(root->left);  // SegFault if root is NULL
+        if(index < left_size){
+            auto rec = split(index, root->left);
+            root->left = rec.second;
+            update(root);
+            return {rec.first, root};
         }
-        print_data(root->left);
-        cout << root->data << " ";
-        print_data(root->right);
+        if(index == left_size){
+            node<T> *left = root->left;
+            root->left = NULL;
+            update(root);
+            return {left, root};
+        }
+        if(index < left_size + root->count){
+            node<T> new_root = new node<T>(root->data, root->prio);
+            new_root->count = left_size + root->count - index;
+            root->count = root->count - new_root->count;
+            new_root->right = root->right;
+            root->right = NULL;
+            update(root);
+            update(new_root);
+            return {root, new_root};
+        }
+        if(index == left_size + root->count){
+            node<T> *right = root->right;
+            root->right = NULL;
+            update(root);
+            return {root, right};
+        }
+        auto rec = split(index - left_size - root->count, root->right);
+        root->right = rec.first;
+        update(root);
+        return {root, rec.second};
     }
-};
+
+    template<typename T = int>
+    node<T> *join(node<T> *left, node<T> *right){
+        if(!(left && right)){
+            return left | right;
+        }
+        if(left->prio > right->prio){
+            left->right = join(left->right, right);
+            update(left);
+            return left;
+        }
+        right->left = join(left, right->left);
+        update(right);
+        return right;
+    }
+
+    template<typename T = int>
+    void print_data(node<T> *root){
+        if(root){
+            print_data(root->left);
+            cout << root->data << " " << root->count << endl;
+            print_data(root->right);
+        }
+    }
+
+} // End of namespace treap
+
+treap::node<int> *root;
 
 int main(){
     /*
@@ -109,11 +230,19 @@ int main(){
     ios_base::sync_with_stdio(false);   // unsync C- and C++-streams (stdio, iostream)
     cin.tie(NULL);  // untie cin from cout (no automatic flush before read)
 
-    int T;
-    cin >> T;
-    loop(t, T){
-        // Solve test case here
+    int n;
+    cin >> n;
+    loop(i, n){
+        int a;
+        cin >> a;
+        root = treap::insert(a, root);
     }
+    loop(j, 5){
+        int a;
+        cin >> a;
+        cout << treap::rank(a, root) << endl;
+    }
+    treap::print_data(root);
 
     /*
     auto stop = chrono::high_resolution_clock::now();
